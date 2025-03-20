992ed39641c095d3102d622c31d0ffb86063f538
Diayu <diayunuraini9@gmail.com>
2025-03-21 00:32:50 +0700

    Remove base path for GitHub Pages root deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/tanivoice/",  // Sesuaikan dengan nama repo
+  base: "/",  // Sesuaikan dengan nama repo
   plugins: [react()],
 });
